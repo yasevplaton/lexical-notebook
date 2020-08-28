92e18a7741cc173dc701505fc925d196e149adc6
yasevplaton <yasevplaton@gmail.com>
2020-08-28 21:33:37 +0300

    build basic layout of main page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.pcss';
-import { Button } from 'antd';
+import { Layout} from 'antd';
+import './styles/base.pcss';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <Button type="primary" className="button-animation">Button</Button>
-    </div>
+    <Layout className="layout">
+      <div className="layout__container">
+        <Header className="header">Header</Header>
+        <Layout className="layout__main">
+          <Sider className="sider">Sider</Sider>
+          <Content className="content">Content</Content>
+        </Layout>
+        <Footer className="footer">Footer</Footer>
+      </div>
+    </Layout>
   );
 }
 

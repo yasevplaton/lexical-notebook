8f7c5a2c8cef6f10f336458777fd3059609cde7a
yasevplaton <yasevplaton@gmail.com>
2020-08-28 09:37:58 +0300

    add antd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
